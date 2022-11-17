fb39e694b57e878b3d566a5d439e4cbc5ea9b83c
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-17 11:47:05 +0100

    Created BankDapp contract, added 1 ether and 1 day constants

---
+++ contract/contracts/LoanDapp.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Loan decentralized appplication
+ */
+contract BankDapp {
+
+    //1 Ether value
+    uint constant ETHER = 1 * 10**18;
+    //1 day in seconds value
+    uint constant DAY = 86400;
+
+}
