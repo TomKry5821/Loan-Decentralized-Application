e196d3bfb292ec4f12335d77abcada16a0f5d309
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-17 11:56:22 +0100

    Added loan struct and loan mapping, cleanup

--- contract/contracts/LoanDapp.sol
+++ contract/contracts/LoanDapp.sol
@@ -6,21 +6,35 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Loan decentralized appplication
  */
 contract BankDapp {
-
     //1 Ether value
-    uint constant ETHER = 1 * 10**18;
+    uint256 constant ETHER = 1 * 10**18;
     //1 day in seconds value
-    uint constant DAY = 86400;
+    uint256 constant DAY = 86400;
 
     //Contract owner wallet address
     address private ownerAddress;
     //Application balance
-    uint private applicationBalance;
+    uint256 private applicationBalance;
+
+    //Map that contains users loans with borrower wallet address as key
+    mapping(address => Loan) private loans;
 
     constructor() payable {
         //Assign contract deployer address as owner address
         ownerAddress = msg.sender;
         //Assign application balance with transaction ether value
         applicationBalance = msg.value;
+    }
+
+    struct Loan {
+        uint256 loanAmount;
+        uint256 amountToBePaid;
+        uint256 remaingAmountToPay;
+        uint256 interest;
+        uint256 installmentAmount;
+        uint256 installmentsNumber;
+        uint256 remainingInstallments;
+        uint256 loanStartDate;
+        uint256 nextInstallmentDate;
     }
 }
