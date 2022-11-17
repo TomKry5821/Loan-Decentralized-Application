f943659fc8813502854381fb0ca32de54fb5b269
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-17 12:58:45 +0100

    Cleanup

--- contract/test/Lock.js
+++ contract/test/Lock.js
@@ -1,10 +1,10 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
-
+//const {
+//  time,
+//  loadFixture,
+//} = require("@nomicfoundation/hardhat-network-helpers");
+//const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+//const { expect } = require("chai");
+//
 //describe("Lock", function () {
 //  // We define a fixture to reuse the same setup in every test.
 //  // We use loadFixture to run this setup once, snapshot that state,
