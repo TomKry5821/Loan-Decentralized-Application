b0440dc7e712dfd2fdf3e3e7fc1ab9ce86a1ba9d
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-17 12:21:30 +0100

    Code corrections

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -8,18 +8,16 @@ const hre = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const etherAmount = hre.ethers.utils.parseEther("10");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const LoanDapp = await hre.ethers.getContractFactory("LoanDapp");
+  const loan = await LoanDapp.deploy(currentTimestampInSeconds, { value: etherAmount });
 
-  await lock.deployed();
+  await loan.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `LoanDapp with 10 ETH and timestamp ${currentTimestampInSeconds} deployed to ${loan.address}`
   );
 }
 
