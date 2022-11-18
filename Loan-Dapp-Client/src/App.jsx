b347c34afbbd3aee84bcf41da758c2982f763e1f
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-18 19:39:43 +0100

    Added new components to App

--- Loan-Dapp-Client/src/App.jsx
+++ Loan-Dapp-Client/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './components/Home'
 import Navbar from './components/navbar'
+import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
-    <Navbar />
+    <>
+      <Router >
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/dashbosard" element={<Dashboard />} />
+        </Routes>
+      </Router >
+    </>
   )
 }
 
