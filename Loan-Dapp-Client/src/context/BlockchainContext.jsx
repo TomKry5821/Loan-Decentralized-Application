67ae7b7c6b7014ae2c59eeaa494ff5ae8f866c03
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-19 00:17:59 +0100

    Added blockchain context with connect wallet, check if wallet is connected and get borrowers balance methods

---
+++ Loan-Dapp-Client/src/context/BlockchainContext.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import { abi, contractAddress } from '../config.json'
+import { ethers } from "ethers"
+import { toast } from 'react-toastify';
+
+export const BlockchainContext = React.createContext("");
+
+export const BlockchainProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState("");
+    //const [balance, setBalance] = useState()
+    //const [renterExists, setRenterExists] = useState()
+    //const [renter, setRenter] = useState()
+    const [borrowerBalance, setBorrowerBalance] = useState()
+    //const [due, setDue] = useState()
+    //const [duration, setDuration] = useState()
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+
+    const address = contractAddress;
+    const contractAbi = abi;
+    const contract = new ethers.Contract(address, contractAbi, signer);
+
+    const connectWallet = async () => {
+        try {
+            if (!window.ethereum) return alert("Please install Metmask")
+
+            const accounts = await provider.send("eth_requestAccounts");
+            console.log(accounts[0])
+            setCurrentAccount(accounts[0])
+        } catch (error) {
+            console.log(error)
+            throw new Error("No ethereum object")
+        }
+    }
+
+    const checkifWalletIsConnected = async () => {
+        try {
+            if (!window.ethereum) return alert("Please install Metamask")
+
+            const accounts = await provider.send("eth_accounts");
+            if (accounts.length) {
+                setCurrentAccount(accounts[0])
+            } else {
+                console.log("No accounts found")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getBorrowerBalance = async () => {
+        try {
+            if (currentAccount) {
+                const balance = await contract.getBorrowerBalance(currentAccount)
+                setBorrowerBalance(ethers.utils.formatEther(balance))
+            }
+        } catch (error) {
+            console.log(error)
+            setBorrowerBalance(ethers.utils.formatEther(0))
+        }
+    }
+
+
+    useEffect(() => {
+        checkifWalletIsConnected()
+        getBorrowerBalance()
+    }, [currentAccount])
+
+
+
+    return (
+        <BlockchainContext.Provider
+            value={{
+                connectWallet,
+                currentAccount,
+                borrowerBalance,
+            }}>
+            {children}
+        </BlockchainContext.Provider>
+    )
+}
