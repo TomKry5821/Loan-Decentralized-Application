e7c6cda625a7613da119e646adb5f4d51c71ece5
TomKry5821 <tomakry729@student.polsl.pl>
2022-12-17 18:14:55 +0100

    Added loan history modal component

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -8,17 +8,37 @@ import {
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
+  RadioGroup,
+  Radio,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Tfoot
+
 } from '@chakra-ui/react';
-import {
-  HamburgerIcon,
-  CloseIcon,
-} from '@chakra-ui/icons';
-import { useContext } from 'react';
+import { useContext, useState, useRef } from 'react';
 import { BlockchainContext } from '../context/BlockchainContext';
+import { ethers } from "ethers"
 
 export default function Navbar() {
-  const { isOpen, onToggle } = useDisclosure();
-  const { connectWallet, currentAccount } = useContext(BlockchainContext)
+  const { connectWallet, currentAccount, loanHistory } = useContext(BlockchainContext)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [scrollBehavior, setScrollBehavior] = useState('inside')
+
+  const btnRef = useRef(null)
+
 
   return (
     <Box>
@@ -32,19 +52,6 @@ export default function Navbar() {
         borderStyle={'solid'}
         borderColor={useColorModeValue('gray.200', 'gray.900')}
         align={'center'}>
-        <Flex
-          flex={{ base: 1, md: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}>
-          <IconButton
-            onClick={onToggle}
-            icon={
-              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-            }
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
-          />
-        </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
           <Text
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
@@ -62,6 +69,22 @@ export default function Navbar() {
           justify={'flex-end'}
           direction={'row'}
           spacing={6}>
+          {
+            !!currentAccount &&
+            <Button
+              onClick={onOpen}
+              display={{ base: 'none', md: 'inline-flex' }}
+              fontSize={'sm'}
+              fontWeight={600}
+              color={'white'}
+              bg={'blue.400'}
+              href={'#'}
+              _hover={{
+                bg: 'blue.300',
+              }}>
+              Loans history
+            </Button>
+          }
           <Button
             onClick={connectWallet}
             display={{ base: 'none', md: 'inline-flex' }}
@@ -77,8 +100,66 @@ export default function Navbar() {
           </Button>
         </Stack>
       </Flex>
-
+      <>
+        <Modal
+          onClose={onClose}
+          finalFocusRef={btnRef}
+          isOpen={isOpen}
+          scrollBehavior={scrollBehavior}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Your loans history</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              {
+                !!loanHistory && loanHistory.map(loan => (
+                  <TableContainer>
+                    <Table variant='simple'>
+                      <TableCaption>Loan stats</TableCaption>
+                      <Thead>
+                        <Tr>
+                          <Th>Information</Th>
+                          <Th>Value</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        <Tr>
+                          <Td>Loan amount</Td>
+                          <Td>{ethers.utils.formatEther(loan.loanAmount).toString()}ETH</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Interest</Td>
+                          <Td>{loan.interest.toString()}%</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Installment number</Td>
+                          <Td >{loan.installmentsNumber.toString()}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Loan start date</Td>
+                          <Td >{new Date(loan.loanStartDate * 1000).toISOString().slice(0, 10)}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Active</Td>
+                          <Td >{loan.isActive? "Yes" : "No"}</Td>
+                        </Tr>
+                      </Tbody>
+                      <Tfoot>
+                      </Tfoot>
+                    </Table>
+                  </TableContainer>
+                ))
+              }
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
     </Box>
+
   );
 }
 
